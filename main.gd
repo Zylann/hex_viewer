9f6f020fd7d5bb28d7fd00564c2903e142f4265a
Marc Gilleron <marc.gilleron@gmail.com>
2019-08-11 20:03:51 +0100

    Open file dialog

--- main.gd
+++ main.gd
@@ -9,16 +9,22 @@ onready var _minimap = get_node("VBoxContainer/Main/Minimap")
 class BufferWrapper:
 	var buffer = PoolByteArray()
 
-var _colors = []
-var _images = []
 var _wrapped_buffer = BufferWrapper.new()
+var _open_dialog = null
 
 
-func _ready():
+func _ready():	
 	_text_view.set_wrapped_buffer(_wrapped_buffer)
-	for i in 256:
-		var col = gradient.interpolate(i / 256.0)
-		_colors.append(col)
+	
+	var base_control = self
+	
+	_open_dialog = FileDialog.new()
+	_open_dialog.mode = FileDialog.MODE_OPEN_FILE
+	_open_dialog.access = FileDialog.ACCESS_FILESYSTEM
+	_open_dialog.connect("file_selected", self, "_on_OpenDialog_file_selected")
+	base_control.add_child(_open_dialog)
+	
+	# TEST
 	#open_file("D:/PROJETS/INFO/GODOT/Plugins/HTerrain/hterrain/addons/zylann.hterrain/hterrain.gd")
 	#open_file("D:/PROJETS/INFO/GODOT/Games/HexViewer/main.gd")
 	open_file("D:/PROJETS/INFO/GODOT/bin/Godot_v3.1-stable_win64.exe")
@@ -32,10 +38,19 @@ func open_file(fpath):
 	if err != OK:
 		printerr("Could not open ", fpath)
 		return null
+	OS.set_window_title(str(fpath, " - Hex Viewer"))
 	var flen = f.get_len()
 	var buffer = f.get_buffer(flen)
 	f.close()
 	_wrapped_buffer.buffer = buffer
 	_minimap.update_textures(buffer)
 	_text_view.update()
+
+
+func _on_OpenButton_pressed():
+	_open_dialog.popup_centered_ratio()
+
+
+func _on_OpenDialog_file_selected(fpath):
+	open_file(fpath)
 
